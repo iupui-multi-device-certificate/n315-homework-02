46490ecf2adec078b50b8e923c55a1038684b8ac
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-09 07:51:51 -0500

    fix: branding & copy

--- app/views/homeView.js
+++ app/views/homeView.js
@@ -1,8 +1,8 @@
 export const homeView = () => `
   <section class="hero">   
       <div class="hero-cta">
-        <h1> Quote Garden</h1>
-        <p> Where wisdom grows</p>
+        <h1>Wisdom Patches</h1>
+        <p>Where wisdom grows</p>
       </div>    
   </section>
 `;
